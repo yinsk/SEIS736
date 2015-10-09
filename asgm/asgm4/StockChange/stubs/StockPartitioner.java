92e986ab85f86e2e01c15a66282a96720b25be81
yinsk <yinsk25@gmail.com>
2015-10-09 16:16:39 -0500

    Update StockPartitioner.java

--- asgm/asgm4/StockChange/stubs/StockPartitioner.java
+++ asgm/asgm4/StockChange/stubs/StockPartitioner.java
@@ -10,8 +10,9 @@ public class StockPartitioner<K2, V2> extends Partitioner<Text, FloatWritable> {
 
   public int getPartition(Text key, FloatWritable value, int numReduceTasks) {
     /*
-     * implement reducers and partitioner
-     *
+     * This partitioner is used in StockChange 
+     * This partitioner will return integer between 0 and 25 base on the first letter of input 
+     * ASCII codes
      */
 	 String s = key.toString();
 	 char c = (char) s.charAt(0);
