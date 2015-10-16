06cf4bfbd14b605221c86a30ebab9a1a312f2375
yinsk <yinsk25@gmail.com>
2015-10-16 16:49:59 -0500

    Update TweetCountDriver.java

--- asgm/asgm5/count/TweetCountDriver.java
+++ asgm/asgm5/count/TweetCountDriver.java
@@ -7,52 +7,26 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 
-/* 
- * MapReduce jobs are typically implemented by using a driver class.
- * The purpose of a driver class is to set up the configuration for the
- * MapReduce job and to run the job.
- * Typical requirements for a driver class include configuring the input
- * and output data formats, configuring the map and reduce classes,
- * and specifying intermediate data formats.
- * 
- * The following is the code for the driver class:
- */
+
 public class TweetCountDriver {
 
 	public static void main(String[] args) throws Exception {
 
-		/*
-		 * The expected command-line arguments are the paths containing input
-		 * and output data. Terminate the job if the number of command-line
-		 * arguments is not exactly 2.
-		 */
+	
 		if (args.length != 2) {
 			System.out.printf("Usage: WordCount <input dir> <output dir>\n");
 			System.exit(-1);
 		}
 
-		/*
-		 * Instantiate a Job object for your job's configuration.
-		 */
+	
 		Job job = new Job();
 
-		/*
-		 * Specify the jar file that contains your driver, mapper, and reducer.
-		 * Hadoop will transfer this jar file to nodes in your cluster running
-		 * mapper and reducer tasks.
-		 */
+	
 		job.setJarByClass(TweetCountDriver.class);
 
-		/*
-		 * Specify an easily-decipherable name for the job. This job name will
-		 * appear in reports and logs.
-		 */
 		job.setJobName("Tweet Count");
 
-		/*
-		 * Specify the paths to the input and output data based on the
-		 * command-line arguments.
-		 */
+	
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
