b918eaad436b052982982d7b6f1d09c5de5c4471
yinsk <yinsk25@gmail.com>
2015-12-29 00:09:29 -0600

    Update guildSortDriver.java

--- project/MR_Java/project/src/guildSort/guildSortDriver.java
+++ project/MR_Java/project/src/guildSort/guildSortDriver.java
@@ -1,7 +1,5 @@
 package guildSort;
 
-
-
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
@@ -19,35 +17,22 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-
-
-
-		
 public class guildSortDriver extends Configured implements Tool {
 
     public int run(String[] args) throws Exception {
-		
-		
-			
-		Job job = new Job();
-		
-		
-		job.setJarByClass(guildSortDriver.class);
-
 	
+		Job job = new Job();
+		job.setJarByClass(guildSortDriver.class);
 		job.setJobName("guild sort");
 
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-	
-		
+
 		job.setMapperClass(guildSortMapper.class);
 		job.setReducerClass(guildSortReducer.class);
 		
 		job.setMapOutputKeyClass(IntWritable.class);
-	    job.setMapOutputValueClass(Text.class);
+	    	job.setMapOutputValueClass(Text.class);
 
 		job.setOutputKeyClass(IntWritable.class);
 		job.setOutputValueClass(Text.class);
@@ -55,9 +40,7 @@ public class guildSortDriver extends Configured implements Tool {
 		job.setSortComparatorClass(IntComparator.class);
 		
 		job.setNumReduceTasks(1);
-	    
-		
-		
+
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
     
