a99eb28a4722e673632f3f633a6f978eeef5ca2d
yinsk <yinsk25@gmail.com>
2015-12-29 00:06:25 -0600

    Update joinDriver.java

--- project/MR_Java/project2/src/joinTwo/joinDriver.java
+++ project/MR_Java/project2/src/joinTwo/joinDriver.java
@@ -17,45 +17,28 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-
-
-
-		
 public class joinDriver extends Configured implements Tool {
 
     public int run(String[] args) throws Exception {
-		
-		
-			
-		Job job = new Job();
-		
-		
-		job.setJarByClass(joinDriver.class);
-
 	
+		Job job = new Job();
+		job.setJarByClass(joinDriver.class);
 		job.setJobName("Join");
 
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-	
-		
+
 		job.setMapperClass(joinMapper.class);
 		job.setReducerClass(joinReducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
-	    job.setMapOutputValueClass(Text.class);
+	    	job.setMapOutputValueClass(Text.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		
-		//job.setSortComparatorClass(IntComparator.class);
-		
 		job.setNumReduceTasks(1);
-	    
-		
-		
+
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
     
