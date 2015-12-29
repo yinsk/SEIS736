b0e3202ec67ab6b655c4e171ffbfb365ed278a3b
yinsk <yinsk25@gmail.com>
2015-12-29 00:08:34 -0600

    Update guildDriver.java

--- project/MR_Java/project/src/guild/guildDriver.java
+++ project/MR_Java/project/src/guild/guildDriver.java
@@ -17,45 +17,26 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-
-
-
-		
 public class guildDriver extends Configured implements Tool {
 
     public int run(String[] args) throws Exception {
-		
-		
-			
-		Job job = new Job();
-		
-		
-		job.setJarByClass(guildDriver.class);
-
 	
+		Job job = new Job();
+		job.setJarByClass(guildDriver.class);
 		job.setJobName("guild count");
 
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-	
-		
+
 		job.setMapperClass(guildMapper.class);
 		job.setReducerClass(guildReducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
-	    job.setMapOutputValueClass(IntWritable.class);
+	    	job.setMapOutputValueClass(IntWritable.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
-		//job.setSortComparatorClass(IntComparator.class);
-		
-		//job.setNumReduceTasks(1);
-	    
-		
-		
+	
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
     
