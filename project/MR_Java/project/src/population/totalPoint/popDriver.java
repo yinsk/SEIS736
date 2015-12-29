04379dd8a24e892e2f6d4f3a29b3920bcbca14c5
yinsk <yinsk25@gmail.com>
2015-12-29 00:12:19 -0600

    Update popDriver.java

--- project/MR_Java/project/src/population/totalPoint/popDriver.java
+++ project/MR_Java/project/src/population/totalPoint/popDriver.java
@@ -17,42 +17,28 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-
-		
 public class popDriver extends Configured implements Tool {
 
     public int run(String[] args) throws Exception {
-		
-		
-			
-		Job job = new Job();
-		
-		
-		job.setJarByClass(popDriver.class);
-
 	
+		Job job = new Job();
+		job.setJarByClass(popDriver.class);
 		job.setJobName("fatcion point count");
 
-		
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-	
-		
+
 		job.setMapperClass(popMapper.class);
 		job.setReducerClass(popReducer.class);
 		
 		job.setMapOutputKeyClass(Text.class);
-	    job.setMapOutputValueClass(IntWritable.class);
+	    	job.setMapOutputValueClass(IntWritable.class);
 
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
-		
-		job.setNumReduceTasks(1);
-	    
 
-		
+		job.setNumReduceTasks(1);
+
 		return (job.waitForCompletion(true) ? 0 : 1);
 	}
     
